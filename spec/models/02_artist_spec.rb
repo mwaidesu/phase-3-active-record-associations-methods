2b6b947b2b58893d3a1c1f45281a4ca74101811c
Mwai <machariamwai@gmail.com>
2022-07-02 18:35:53 +0300

    Done

--- spec/models/02_artist_spec.rb
+++ spec/models/02_artist_spec.rb
@@ -1,8 +1,6 @@
 describe Artist do
-  
   it 'has a name' do
-    prince = Artist.create(name: "Prince")
-    expect(Artist.find_by(name: "Prince")).to eq(prince)
+    prince = Artist.create(name: 'Prince')
+    expect(Artist.find_by(name: 'Prince')).to eq(prince)
   end
-
 end
